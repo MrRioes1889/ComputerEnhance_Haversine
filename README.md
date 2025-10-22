e07affe1bcf3cfd1e1ba4efad58ad1fdceac08e8
MrRioes1889 <armin2.hintze@gmail.com>
2025-10-22 14:08:59 +0200

    Project setup

---
+++ README.md
@@ -0,0 +1,1 @@
+Minimal Project Setup for using VSCode and its CMake Tools extension in conjunction with CMake.
---
+++ haversine/sauce/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	printf_s("Hello, World!\n");
+	return 0;
+}
