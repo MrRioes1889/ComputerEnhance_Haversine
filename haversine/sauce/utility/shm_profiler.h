3d7139c4d850a43af75a7598693b04f331d94173
MrRioes1889 <armin2.hintze@gmail.com>
2025-12-01 03:20:08 +0100

    removed superflous profiler macro

--- haversine/sauce/utility/shm_profiler.h
+++ haversine/sauce/utility/shm_profiler.h
@@ -12,8 +12,7 @@ void __shm_timer_end();
 
 #define __CONCAT2(A, B) A##B
 #define __CONCAT(A, B) __CONCAT2(A, B)
-#define __SHM_TIMER_START2(key, id_var) static uint32 id_var = 0; if(! id_var ) id_var = __shm_timer_get_next_timer_id(); __shm_timer_start(key, id_var)
-#define __SHM_TIMER_START(key, tu_id) __SHM_TIMER_START2(key, __CONCAT(__shm_timer_id_, tu_id))
-#define SHM_TIMER_START_FUNC() __SHM_TIMER_START(__func__, __COUNTER__)
-#define SHM_TIMER_START(key) __SHM_TIMER_START(key, __COUNTER__)
+#define __SHM_TIMER_START(key, id_var) static uint32 id_var = 0; if(! id_var ) id_var = __shm_timer_get_next_timer_id(); __shm_timer_start(key, id_var)
+#define SHM_TIMER_START_FUNC() __SHM_TIMER_START(__func__, __CONCAT(__shm_timer_id_, __COUNTER__))
+#define SHM_TIMER_START(key) __SHM_TIMER_START(key, __CONCAT(__shm_timer_id_, __COUNTER__))
 #define SHM_TIMER_STOP() __shm_timer_end()
